2790ad0ef10615c5565645d690eadcfb414ad342
wangshuihu <wangshuihu7@163.com>
2019-11-25 15:06:00 +0800

    http状态码跟新

--- src/main/java/com/jhbim/bimvr/stauts/enums/StatusEnum.java
+++ src/main/java/com/jhbim/bimvr/stauts/enums/StatusEnum.java
@@ -19,8 +19,6 @@ public enum StatusEnum {
     UPDATE_SUCCESS("200", "数据更新成功"),
     DELETE_SUCCESS("200", "数据删除成功"),
     SELECT_SUCCESS("200", "数据查询成功"),
-    OPEN_SUCCESS("200","菜单启用成功"),
-    DIS_SUCCESS("200","菜单禁用成功"),
 
     SAVE_FAILURE("400", "数据保存失败"),
     ADD_FAILURE("400", "数据添加失败"),
@@ -29,24 +27,9 @@ public enum StatusEnum {
     UPDATE_FAILURE("400", "数据更新失败"),
     DELETE_FAILURE("400", "数据删除失败"),
     SELECT_FAILURE("400", "数据查询失败"),
-    OPEN_FAILURE("400","菜单启用失败"),
-    DIS_FAILURE("400","菜单禁用失败"),
 
-    INVALID_PARAM("400", "请求参数不正确"),
 
-    SIGNATURE_FAILURE("408", "登录验签失败，请重新登录"),
-    EXPIRED_TOKEN("408", "登录超时，请重新登录"),
-    CHECK_TOKEN_FAILURE("408", "登录验证失败，请重新登录"),
-    ILLEGAL_TOKEN("408", "登录验证失败，请重新登录"),
-
-    UPDATE_PASSWORD_FAILURE("400", "原密码不正确"),
-
-    MAD_CODE_EXIST("401", "基础资料code已在单位级存在"),
-
-    GAL_BATCH_PRINT_SUCCESS("402", "打印数据获取中"),
-
-    GAL_BATCH_PRINT_FAILURE("403", "请保存打印方案");
-
+    INVALID_PARAM("400", "非法请求");
 
     private String code;
     private String msg;
